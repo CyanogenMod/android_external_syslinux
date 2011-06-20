ec6f19ef4f5b7ae3466674f9e789ea680ca9527d
Andrew Boie <andrew.p.boie@intel.com>
2011-06-20 14:26:20 -0700

    Android.mk: Use SYSLINUX as INSTALLED_BOOTLOADER_MODULE
    Since we will be using standard Android bootimages for the various
    kernels, we need to create a partition which just contains the
    SYSLINUX binaries and configuration.
    
    This logic used to be handled by SYSLINUX hacks to Android build/,
    but we can now leverage the normal mechanisms for creating the
    bootloader image.
    
    Change-Id: Ica78c666ab22940ba09cc923dedb2b063c1d96b9
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,30 @@
-ifeq ($(TARGET_BOOTIMAGE_TYPE),syslinux)
+ifeq ($(TARGET_USE_SYSLINUX),true)
 
 include $(all-subdir-makefiles)
 
-endif # TARGET_BOOTIMAGE_TYPE==syslinux
+SYSLINUX_BIN := $(HOST_OUT_EXECUTABLES)/syslinux
+SYSLINUX_BASE := $(HOST_OUT)/usr/lib/syslinux
+
+SYSLINUX_MK_IMG := external/syslinux/utils/android-image.sh
+
+# TARGET_SYSLINUX_FILES - any splash screens, com32 modules
+# TARGET_SYSLINUX_CONFIG - file to use as syslinux.cfg
+# These should be defined in BoardConfig.mk per-product
+
+$(INSTALLED_BOOTLOADER_MODULE): \
+		$(TARGET_SYSLINUX_FILES) \
+		$(TARGET_SYSLINUX_CONFIG) \
+		$(SYSLINUX_BIN) \
+		$(SYSLINUX_MK_IMG)
+	$(call pretty, "Target SYSLINUX image: $@")
+	$(SYSLINUX_MK_IMG) \
+		--syslinux $(SYSLINUX_BIN) \
+		--tmpdir $(call intermediates-dir-for,EXECUTABLES,syslinux-img)/syslinux-img \
+		--config $(TARGET_SYSLINUX_CONFIG) \
+		--output $@ \
+		$(TARGET_SYSLINUX_FILES)
+
+.PHONY: syslinux-image
+syslinux-image: $(INSTALLED_BOOTLOADER_MODULE)
+
+endif
